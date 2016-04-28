1fcf7dd79cdc9497910167914aa0d7b0c734a89d
Max Hutchinson <maxhutch@gmail.com>
2016-04-28 17:42:38 -0500

    Trying to add a real test.
    I'd be pretty shocked if this worked.  Probably missing a dep.

---
+++ nekpy/tests/test_sweep.py
@@ -0,0 +1,31 @@
+import json
+from os import getcwd
+from nekpy.dask.subgraph import series
+from nekpy.dask.utils import outer_product, work_name
+from nekpy.dask import run_all
+from nekpy.dask.tasks import configure
+from os.path import join, dirname
+
+def test_sweep():
+
+    with open(join(dirname(__file__), "LST.json"), "r") as f:
+        base = json.load(f)
+
+    with open(join(dirname(__file__), "LST.sweep"), "r") as f:
+        sweeps = json.load(f)
+
+    with open(join(dirname(__file__), "LST.tusr"), "r") as f:
+        tusr = f.read()
+
+    base["prefix"] = sweeps["prefix"]
+    del sweeps["prefix"]
+
+    overrides = list(outer_product(sweeps))
+    for ov in overrides:
+        ov["name"] = work_name(base["prefix"], ov)
+
+    workdirs = [join(getcwd(), x["name"]) for x in overrides]
+    configs = [configure(base, override, workdir) for override, workdir in zip(overrides, workdirs)]
+    res = [series(config, tusr, job_step = 25) for config in configs]
+
+    final = run_all(res, base)
